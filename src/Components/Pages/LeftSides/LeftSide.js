f2378b2c2b853458cac33b8eb44174d0b86a8492
Raihan <rhnchdri@gmail.com>
2023-03-15 11:51:10 +0600

    some added frnd

--- src/Components/Pages/LeftSides/LeftSide.js
+++ src/Components/Pages/LeftSides/LeftSide.js
@@ -3,23 +3,24 @@ import { RxDotsHorizontal } from "react-icons/rx";
 import { FaEdit } from "react-icons/fa";
 import { BiSearch } from "react-icons/bi";
 import img1 from '../../../Components/assets/msc-2.jpg';
+import img2 from '../../../Components/assets/msc.jpg';
 
 const LeftSide = () => {
     return (
         <div>
             <div className='flex justify-between'>
-                <div className="avatar">
+                <div className="avatar space-x-4">
                     <div className="w-8 rounded-full">
                         <img src={img1} alt='' />
-                        <p className='text-red'>Raihan</p>
                     </div>
+                    <p className='text-red'>Raihan</p>
                 </div>
                 <div className='flex justify-between'>
                     <span><RxDotsHorizontal></RxDotsHorizontal></span>
                     <FaEdit></FaEdit>
                 </div>
             </div>
-            <div className='flex rounded-full p-1 border-2'>
+            <div className='flex rounded-full p-1 border-2 my-4'>
                 <button><BiSearch></BiSearch></button>
                 <input type="text" placeholder='Search' className='ml-2 form-control border-none rounded-none' />
             </div>
@@ -48,6 +49,68 @@ const LeftSide = () => {
                     <div className="w-8 rounded-full">
                         <img src={img1} alt='' />
                     </div>
+                </div>
+            </div>
+            <div className='grid gap-3'>
+                <div className="avatar space-x-4 hover:bg-slate-500">
+                    <div className="w-8 rounded-full">
+                        <img src={img2} alt='' />
+                    </div>
+                    <p className='text-red'>Raihan</p>
+                </div>
+                <div className="avatar space-x-4 hover:bg-slate-500">
+                    <div className="w-8 rounded-full">
+                        <img src={img2} alt='' />
+                    </div>
+                    <p className='text-red'>Raihan</p>
+                </div>
+                <div className="avatar space-x-4 hover:bg-slate-500">
+                    <div className="w-8 rounded-full">
+                        <img src={img2} alt='' />
+                    </div>
+                    <p className='text-red'>Raihan</p>
+                </div>
+                <div className="avatar space-x-4 hover:bg-slate-500">
+                    <div className="w-8 rounded-full">
+                        <img src={img2} alt='' />
+                    </div>
+                    <p className='text-red'>Raihan</p>
+                </div>
+                <div className="avatar space-x-4 hover:bg-slate-500">
+                    <div className="w-8 rounded-full">
+                        <img src={img2} alt='' />
+                    </div>
+                    <p className='text-red'>Raihan</p>
+                </div>
+                <div className="avatar space-x-4 hover:bg-slate-500">
+                    <div className="w-8 rounded-full">
+                        <img src={img2} alt='' />
+                    </div>
+                    <p className='text-red'>Raihan</p>
+                </div>
+                <div className="avatar space-x-4 hover:bg-slate-500">
+                    <div className="w-8 rounded-full">
+                        <img src={img2} alt='' />
+                    </div>
+                    <p className='text-red'>Raihan</p>
+                </div>
+                <div className="avatar space-x-4 hover:bg-slate-500">
+                    <div className="w-8 rounded-full">
+                        <img src={img2} alt='' />
+                    </div>
+                    <p className='text-red'>Raihan</p>
+                </div>
+                <div className="avatar space-x-4 hover:bg-slate-500">
+                    <div className="w-8 rounded-full">
+                        <img src={img2} alt='' />
+                    </div>
+                    <p className='text-red'>Raihan</p>
+                </div>
+                <div className="avatar space-x-4 hover:bg-slate-500">
+                    <div className="w-8 rounded-full">
+                        <img src={img2} alt='' />
+                    </div>
+                    <p className='text-red'>Raihan</p>
                 </div>
             </div>
         </div>
